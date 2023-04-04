95de7b7124c2d9609a7394945279dbe3ee2c9f62
gagandeepsingh21 <gagandeepgahir@gmail.com>
2023-04-04 16:49:28 +0300

    minor adjustments

--- resources/views/vendor/filament-breezy/login.blade.php
+++ resources/views/vendor/filament-breezy/login.blade.php
@@ -20,19 +20,7 @@
     <div class="w-full flex justify-center">
         <x-filament::brand />
     </div>
-    <div>
-        <h2 class="font-bold tracking-tight text-center text-2xl">
-            {{ __('filament::login.heading') }}
-        </h2>
-        @if(config("filament-breezy.enable_registration"))
-        <p class="mt-2 text-sm text-center">
-            {{ __('filament-breezy::default.or') }}
-            <a class="text-primary-600" href="{{route(config('filament-breezy.route_group_prefix').'register')}}">
-                {{ strtolower(__('filament-breezy::default.registration.heading')) }}
-            </a>
-        </p>
-        @endif
-    </div>
+
 
     {{ $this->form }}
 
