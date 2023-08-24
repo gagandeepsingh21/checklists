032051f623b33ce5364609b70074655a2fe95bb3
gagandeepsingh21 <gagandeepgahir@gmail.com>
2023-08-24 09:18:27 +0300

    changes

---
+++ database/migrations/2023_08_15_223240_create_checklists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checklists', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->default(0);
+            $table->longText('message')->nullable();
+            $table->date('date_resolved')->nullable();
+            $table->string('status')->nullable();
+            $table->string('resolved_by')->nullable();
+            $table->date('date_created')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checklists');
+    }
+};
