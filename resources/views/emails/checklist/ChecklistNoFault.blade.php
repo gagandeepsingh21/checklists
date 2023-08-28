f1f46d481da22e9b260c2b81e8d5d19c7217b2c8
gagandeepsingh21 <gagandeepgahir@gmail.com>
2023-08-28 17:16:20 +0300

    changes

---
+++ resources/views/emails/checklist/ChecklistNoFault.blade.php
@@ -0,0 +1,27 @@
+<x-mail::message>
+<h1>Checklist Created</h1>
+<p>Greetings {{ Auth::user()->name}},</p>
+
+
+<p>Your Checklist has been created on <b>{{ $date_created }}</b>.
+    Please have a look below for more information.
+</p>
+@component('mail::table')
+|                   |                                      |
+| ----------------- | ------------------------------------ |
+| **Building name** | {{ $building_name }}                  |
+| **Class Name**    | {{ implode(',' ,$class_name) }}        |
+| **Message**       | {{ $message }}                         |
+| **Status**        | {{ $status }}                          |
+| **Date Created**  | {{ $date_created }}                    |
+@endcomponent
+
+
+<x-mail::button :url=$link>
+View Checklist
+</x-mail::button>
+
+Best Regards,<br>
+<i>ICT CheckList Team</i>
+<!-- {{ config('app.name') }} -->
+</x-mail::message>
