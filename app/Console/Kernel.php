cb811ba976eaa2ec15b9d7b1da8ebf103e809e98
gagandeepsingh21 <gagandeepgahir@gmail.com>
2023-09-19 10:04:18 +0300

    changed kernel for every2days emails

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->job(new mails())->weekly();
+        $schedule->job(new mails())->everyTwoDays();
     }
 
     /**
